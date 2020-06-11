26c93ee24be277042f8788d86a94eedf4f6e4861
Ainhoa Izkue <ainhoa.izkue@biko2.com>
2020-06-11 16:11:28 +0200

    implementa fizzbuzz devolviendo el primer elemento

--- test/fizzBuzz.test.js
+++ test/fizzBuzz.test.js
@@ -1,1 +1,16 @@
 // Test para kata fizz buzz
+describe('Kata Fizz Buzz', () => {
+  it('el primer elemento debería ser 1', () => {
+    //Arrange
+
+    //Act
+    const outputFB = fizzBuzz()
+
+    //Assert
+    expect(outputFB[0]).toBe('1')
+  })
+})
+
+function fizzBuzz() {
+  return '1'
+}
