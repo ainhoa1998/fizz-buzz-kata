b21a962dc0a11f53bf7754bd454b11030842d197
Ainhoa Izkue <ainhoa.izkue@biko2.com>
2020-06-15 16:22:51 +0200

    Refactor: elimina codigo duplicado

--- src/calculateNumber.js
+++ src/calculateNumber.js
@@ -1,10 +1,7 @@
 export function calculateNumber(number) {
-  if (number === 3) {
+  if (number === 3 || number === 6) {
     return 'Fizz'
   }
 
-  if (number === 6) {
-    return 'Fizz'
-  }
   return number
 }
