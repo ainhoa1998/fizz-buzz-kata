56068222827ed45b6f7e5ead13be17f497931400
Ainhoa Izkue <ainhoa.izkue@biko2.com>
2020-06-15 17:15:25 +0200

    Refactor: extrae a fichero

--- test/fizzBuzz.test.js
+++ test/fizzBuzz.test.js
@@ -1,11 +1,9 @@
+import { fizzBuzz } from '../src/fizzBuzz'
+
 describe('Kata Fizz Buzz', () => {
-  it('Devuelve un string con todos los números del 1 al 100 cambiando los múltiplos de 3 por Fizz, los de 5 por Buzz y los de 3 y 5 por FizzBuzz', () => {
+  it('Comprueba primer elemento', () => {
     const output = fizzBuzz().split('\n')
 
     expect(output[0]).toBe('1')
   })
 })
-
-function fizzBuzz() {
-  return '1'
-}
