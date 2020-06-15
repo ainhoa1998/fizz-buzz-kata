2fd42a1edbc9153ec4516c5e0d15cc1c77c0cb05
Ainhoa Izkue <ainhoa.izkue@biko2.com>
2020-06-15 16:04:16 +0200

    ignore: elimina comentario

--- test/calculateNumber.test.js
+++ test/calculateNumber.test.js
@@ -2,7 +2,6 @@ import { calculateNumber } from '../src/calculateNumber.js'
 describe('Kata Fizz Buzz', () => {
   it('Si el número no es múltiplo de 3 ni de 5, devuelve el mismo número', () => {
     const output = calculateNumber(4)
-    // que el número devuelto es 4
     expect(output).toBe(4)
   })
 })
