7a12c1264a562386a44aed6eda3a23611cf72895
Ainhoa Izkue <ainhoa.izkue@biko2.com>
2020-06-15 16:06:03 +0200

    ignore: añade saltos de linea

--- test/calculateNumber.test.js
+++ test/calculateNumber.test.js
@@ -1,7 +1,9 @@
 import { calculateNumber } from '../src/calculateNumber.js'
+
 describe('Kata Fizz Buzz', () => {
   it('Si el número no es múltiplo de 3 ni de 5, devuelve el mismo número', () => {
     const output = calculateNumber(4)
+
     expect(output).toBe(4)
   })
 })
